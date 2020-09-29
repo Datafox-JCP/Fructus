bc2f836db6a43551f7ebb9c66b6ed522db45aa32
Datafox_JCP <datafox@outlook.com>
2020-09-29 14:25:43 -0500

    Initial Commit

---
+++ Fructus/App/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Fructus
+//
+//  Created by Juan Carlos on 27/07/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // MARK: - Properties
+
+    var fruits: [Fruit] = fruitsData
+
+    // MARK: - Body
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(fruits.shuffled()) { item in
+                    NavigationLink(destination: FruitDetailView(fruit: item)) {
+                        FruitRowView(fruit: item)
+                            .padding(.vertical, 4)
+                    }
+                }
+            }
+            .navigationTitle("Fruits")
+        } //: Navigation
+    }
+}
+
+// MARK: - Preview
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(fruits: fruitsData)
+            .previewDevice("iPhone 11 Pro")
+    }
+}
